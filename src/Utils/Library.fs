3673045ef3f38f37dbce58e93b1b16fcb0933b8d
Michael Wiinberg Olesen <michael.w.olesen@outlook.com>
2020-01-04 14:35:56 +0100

    Added Xml comments

--- src/Utils/Library.fs
+++ src/Utils/Library.fs
@@ -6,21 +6,29 @@ open SFX.Utils.Infrastructure
 module Time =
 
     let private dateTimeProvider = DateTimeProvider()
+    /// Creates a DateTimeProvider
     let createDateTimeProvider = DateTimeProvider
     let private timeZoneProvider = TimeZoneProvider()
+    /// Creates a TimeZoneProvider
     let createTimeZoneProvider = TimeZoneProvider
     let private dateTimeConverter = DateTimeConverter(timeZoneProvider)
-    let createDateTimeConverter = DateTimeConverter
     
+    /// Gets the local "now" (DateTimeOffset)
     let getNow = dateTimeProvider.GetNow
+    /// Gets "now" in Utc (DateTimeOffset)
     let getUtcNow = dateTimeProvider.GetUtcNow
 
+    /// Gets the local TimeZoneInfo
     let getLocalTimeZone = timeZoneProvider.GetLocal
+    /// Gets the Utc TimeZomeInfo
     let getUtcTimeZone = timeZoneProvider.GetUtc
+    /// Attempts to find the TimeZoneInfo denoted by the argument
     let findSystemTimeZoneById = 
         timeZoneProvider.FindSystemTimeZoneById >> toResult
 
+    /// Converts dt to a DateTimeOffset by tz
     let convert dt tz = dateTimeConverter.Convert(dt, tz) |> toResult
+    /// Converts the argument to Utc
     let toUtc = dateTimeConverter.ToUtc >> toResult
 
 module Timer =
@@ -28,14 +36,22 @@ module Timer =
     open System
 
     let private timerProvider = TimerProvider()
+    /// Creates a TimerProvider
     let createTimerProvider = TimerProvider
 
+    /// Creates a Timer running with the interval denoted invoking the handler
+    /// provided upon each firing of the timer. The parameter autoStart denotes
+    /// whether to automatically start the timer
     let createTimer interval (handler: unit -> unit) autoStart =
         timerProvider.Create(interval, System.Action(handler), autoStart) |> toResult
 
+    /// Rperesents the errors, that can occur when working with timers
     type TimerError =
+    /// The timer provided is null
     | Null
+    /// The timer provided is already disposed
     | TimerDisposed
+    /// An exception has occurred
     | Other of exn
 
     let private toResult (x: SFX.ROP.CSharp.Result<'a>) =
@@ -43,27 +59,39 @@ module Timer =
         else 
             if (x.Error :?> ObjectDisposedException) <> null then TimerDisposed |> fail
             else x.Error |> Other |> fail
+    /// Starts the timer
     let startTimer (timer: ITimer) =
         if timer |> isNull then Null |> fail
         else timer.Start() |> toResult
+    /// Stops the timer
     let stopTimer (timer: ITimer) =
         if timer |> isNull then Null |> fail
         else timer.Stop() |> toResult
+    // Closes (disposes) the timer
     let closeTimer (timer: ITimer) = 
         if timer |> isNull then Null |> fail
         else timer.Dispose() |> succeed
 
 module Initializable =
+    /// Represents an initializable object
     type InitializableObject =
+    /// The initializable object is IInitializable
     | Sync of IInitializable
+    /// The initializable object is IAsyncInitializable
     | Async of IAsyncInitializable
+    /// Converts an IInitializable to an InitializableObject (Sync)
     let sync = Sync
+    /// Converts an IAsyncInitializable to an InitializableObject (Async)
     let async = Async
 
+    /// Represents the errors that can occur when working with InitializableObjects
     type InitializationError =
+    /// An error occurred while initializing an object
     | InitializationFailed of exn
+    /// An error occurred while checking an object's initialization state
     | InitializationCheckFailed of exn
 
+    /// Initializes an InitializableObject
     let initialize =
         let doInitialize x =
             match x with
@@ -72,6 +100,7 @@ module Initializable =
         let handleError error = error |> InitializationFailed
         tryCatch doInitialize handleError
 
+    /// Checks whether an InitializableObject is initialized
     let isInitialized =
         let doIsInitilized x =
             match x with
@@ -81,10 +110,18 @@ module Initializable =
         tryCatch doIsInitilized handleError
 
 module Initializer =
+    /// Represents an object that can perform initialization (of something other than itself)
     type InitializerObject =
+    /// The initializable object is IInitializer
     | Sync of IInitializer
+    /// The initializable object is IAsyncInitializer
     | Async of IAsyncInitializer
+    /// Converts an IInitializer to an InitializerObject (Sync)
+    let sync = Sync
+    /// Converts an IAsyncInitializer to an InitializerObject (Async)
+    let async = Async
     
+    /// Performs initialization
     let initialize =
         let doInitialize x =
             match x with
@@ -93,8 +130,11 @@ module Initializer =
         tryCatch doInitialize id        
 
 module HashHelpers =
+    /// Represents the errors that can occur when computing hashes
     type HashError = 
+    /// An argument is null
     | Null
+    /// A sequence is empty
     | Empty
 
     let inline private hash' prime1 prime2 x = 
@@ -110,14 +150,19 @@ module HashHelpers =
     let inline private hash'''' (convert: 'a -> int) x =
         x |> Array.map convert |> hash'''
 
+    /// Represents arrays to the hashing functions
     type HashArgs =
+    /// An array of bytes
     | B of byte array
+    /// An array of integers
     | I of int array
+    /// An array of objects
     | O of obj array
     let byteArray = B
     let intArray = I
     let objArray = O
 
+    /// Computes the hash of the provided argument (an array)
     let hash x =
         match x with
         | B x -> x |> hash'''' (fun b -> int(b))
@@ -130,5 +175,7 @@ module HashHelpers =
 module StringHelpers =
     open System
 
+    /// Shortcut for String.IsNullOrEmpty
     let isNullOrEmpty = String.IsNullOrEmpty
+    /// Shortcut for String.IsNullOrWhiteSpace
     let isNullOrWhiteSpace = String.IsNullOrWhiteSpace
